23d2846a17f6f04b1583cc9faa6bf539f258d877
alba mar <albamrquz3@gmail.com>
2023-11-16 15:03:52 +0100

    cors

--- db.js
+++ db.js
@@ -4,6 +4,6 @@ const SUPABASE_URL = 'https://ykuoaqjmfawmzwnvcgrd.supabase.co';
 const SUPABASE_KEY =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlrdW9hcWptZmF3bXp3bnZjZ3JkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAxMjYwMjcsImV4cCI6MjAxNTcwMjAyN30.HhIGo7D2cTtLLKMhA2x9Ul0m9D75pyEn2FCEIE5-3Fo';
 
-const db = createClient(SUPABASE_URL, SUPABASE_KEY);
+const dbConnection = createClient(SUPABASE_URL, SUPABASE_KEY);
 
-export default db;
+export default dbConnection;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 
-import db from './db.js';
+import dbConnection from './db.js';
 
 const app = express();
 const port = 3000;
@@ -9,14 +9,21 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Mehtods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.get('/monuments', async (req, res) => {
-  const { data, error } = await db.from('monument').select('*');
+  const { data, error } = await dbConnection.from('monument').select('*');
   console.log(data);
   res.send(data);
 });
 
 app.get('/users', async (req, res) => {
-  const { data, error } = await db.from('user').select('*');
+  const { data, error } = await dbConnection.from('user').select('*');
   console.log(data);
   res.send(data);
 });
